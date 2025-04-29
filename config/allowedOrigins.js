51a4bd53584c47f1d494795abb371ba53c8a6845
Antony Reyes <antony.reyespilo@gmail.com>
2025-04-29 12:35:34 -0500

    Added database connection and API files

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://humdrum-nutritious-workshop.glitch.me'
+    'https://alive-pastoral-mailbox.glitch.me'
 ];
 
 module.exports = allowedOrigins;
